695286465c033f6fc10f039b5bc69a78b758199b
Chris McDonald <chris@stickyworld.com>
2018-09-21 11:21:55 +0100

    Update setup.py to fix pip.req issue

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements("requirements.txt", session=False)
 reqs = [str(each.req) for each in install_reqs if each.req]
