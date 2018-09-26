3c6616ed8e66fb1e43bb429c6fa7246c5652e2c2
Christopher McDonald <chrismcband@gmail.com>
2018-09-26 15:43:27 +0100

    Attempt to fix signals import.

--- src/doh/apps.py
+++ src/doh/apps.py
@@ -1,10 +1,11 @@
 # http://stackoverflow.com/questions/2719038/where-should-signal-handlers-live-in-a-django-project
 from django.apps import AppConfig
 
+
 class DOHConfig(AppConfig):
     name = 'doh'
     verbose_name = "Django Object Hooks"
 
     def ready(self):
         super(DOHConfig, self).ready()
-        import signals
+        import .signals
